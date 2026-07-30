e27996a552e4ac03b4a3be7aae375b7dcbfee6a2
Satyajeet Kumar <satyajeetdps@gmail.com>
2026-07-30 17:06:21 +0530

    Updated training

--- training/train.py
+++ training/train.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
+import os
 from training.dataset import ChessDataset
 from training.model import ChessPolicyNet
 
@@ -11,9 +11,12 @@ BATCH_SIZE = 256
 LEARNING_RATE = 1e-3
 NUM_EPOCHS = 1
 
+CHECKPOINT_DIR = "/kaggle/working/models"
+CHECKPOINT_FILE = os.path.join(CHECKPOINT_DIR, "latest_checkpoint.pth")
 
+os.makedirs(CHECKPOINT_DIR, exist_ok=True)
 # Dataset
-dataset = ChessDataset("data/processed/positions.csv")
+dataset = ChessDataset("/kaggle/input/datasets/satyajeet69/positions-csv/positions.csv")
 
 loader = DataLoader(
     dataset,
@@ -38,11 +41,23 @@ optimizer = torch.optim.Adam(
     lr=LEARNING_RATE
 )
 
+start_epoch = 0
 
+if os.path.exists(CHECKPOINT_FILE):
+    checkpoint = torch.load(CHECKPOINT_FILE, map_location=device)
+
+    model.load_state_dict(checkpoint["model_state"])
+    optimizer.load_state_dict(checkpoint["optimizer_state"])
+
+    start_epoch = checkpoint["epoch"] + 1
+
+    print(f"Resuming from epoch {start_epoch}")
+else:
+    print("No checkpoint found. Starting fresh.")
 # Training loop
 model.train()
 
-for epoch in range(NUM_EPOCHS):
+for epoch in range(start_epoch, NUM_EPOCHS):
 
     running_loss = 0.0
 
@@ -78,5 +93,14 @@ for epoch in range(NUM_EPOCHS):
                 f"Batch [{batch_idx}] "
                 f"Loss: {avg_loss:.4f}"
             )
-torch.save(model.state_dict(), "models/policy_net_epoch1.pth")
+        if batch_idx % 5000 == 0 and batch_idx != 0:
+            torch.save(
+                model.state_dict(),
+                f"/kaggle/working/models/checkpoint_{batch_idx}.pth"
+            )
+            print(f"Checkpoint saved at batch {batch_idx}")
+torch.save(
+    model.state_dict(),
+    "/kaggle/working/models/policy_net_epoch1.pth"
+)
 print("Model saved.")
