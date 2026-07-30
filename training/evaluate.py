b245d6061cc1b1e96a3af9b30199cf212de12090
Satyajeet Kumar <satyajeetdps@gmail.com>
2026-07-30 20:18:42 +0530

    Improved train-val split

--- training/evaluate.py
+++ training/evaluate.py
@@ -5,7 +5,7 @@ from training.dataset import ChessDataset
 from training.model import ChessPolicyNet
 
 MODEL_PATH = "/kaggle/working/models/policy_net_epoch1.pth"
-VAL_PATH = "/kaggle/input/datasets/satyajeet69/positions-csv/positions.csv"
+VAL_PATH = "/kaggle/input/datasets/satyajeet69/validation-set/val.csv"
 
 BATCH_SIZE = 256
 
